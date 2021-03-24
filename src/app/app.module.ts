e2fb996466d9623d52443db252c6ba21807a3fd3
Jay Landro <jaylandro@gmail.com>
2021-03-24 10:44:28 -0700

    Clean up and CSS styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CounterComponent } from "./counter/counter.component";
+import { OutputComponent } from "./output/output.component";
+import { CounterService } from "./services/counter.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CounterComponent, OutputComponent],
+  imports: [BrowserModule],
+  providers: [CounterService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
